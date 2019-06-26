2aaf21b3a2acfa68bcf90421ae110ef0616864d4
GYAN GURU <geekworks2017@gmail.com>
2019-06-26 19:30:49 +0530

    "Create User Mutation"

---
+++ models/user.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema, Mongoose } from "mongoose";
+
+const UserSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true  
+    },
+    email: {
+        type: String
+    },
+    gender: {
+        type: String
+    },
+    items: [{
+        ref: 'item',
+        type: mongoose.Schema.ObjectId,
+        required: true
+    }]
+});
+
+export default mongoose.model('user', UserSchema);
